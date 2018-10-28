7a8522588c3cb33d18eabd499f31d80c0072b3ed
Bernardo Laing <A01206492@itesm.mx>
2018-10-28 15:15:22 -0600

    Heroku config

--- server/models/mongoose.js
+++ server/models/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://blaing:i57400GsKill16%40@ds129821.mlab.com:29821/todoAPP');
 
 module.exports = {mongoose};
