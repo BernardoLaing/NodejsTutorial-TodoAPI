1c1fa6e0d3dd004d5d07c52c06c5b42e2f89d314
Bernardo Laing <A01206492@itesm.mx>
2018-10-28 15:35:15 -0600

    Fixed mlab uri

--- server/models/mongoose.js
+++ server/models/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://blaing:i57400GsKill16%40@ds129821.mlab.com:29821/todoapp');
+mongoose.connect('mongodb://blaing:i57400GsKill16%40@ds243963.mlab.com:43963/todoapp');
 
 module.exports = {mongoose};
