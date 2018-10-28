4c50e8f74be5fe80d25eac85004c3bc0573ae401
Bernardo Laing <A01206492@itesm.mx>
2018-10-28 15:28:56 -0600

    change database name

--- server/models/mongoose.js
+++ server/models/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://blaing:i57400GsKill16%40@ds129821.mlab.com:29821/todoAPP');
+mongoose.connect('mongodb://blaing:i57400GsKill16%40@ds129821.mlab.com:29821/todoapp');
 
 module.exports = {mongoose};
