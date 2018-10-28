cac7a028a69461c6271cc23ae9c3a36f1a0a848c
Bernardo Laing <A01206492@itesm.mx>
2018-10-28 16:05:09 -0600

    DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -46,6 +46,21 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndDelete(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    return res.send({todo});
+  }).catch((err) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
 });
